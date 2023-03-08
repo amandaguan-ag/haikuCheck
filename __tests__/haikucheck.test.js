c33dd6f6d6fcef6e13773b8103ed673b1a5e77d4
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-07 19:38:23 -0800

    be in red phase for checkline() test

--- __tests__/haikucheck.test.js
+++ __tests__/haikucheck.test.js
@@ -1,14 +1,26 @@
 import HaikuChecker from "./../src/js/haikuCheck.js";
 
-describe("HaikuChecker", function () {
-  test("should correctly create a Haiku object with three lines", function () {
+describe("HaikuChecker", () => {
+  describe("constructor", () => {
+    test("should correctly create a Haiku object with three lines", function () {
+      let haikuCheck = new HaikuChecker(
+        "This is line one",
+        "This is line two",
+        "This is line three"
+      );
+      expect(haikuCheck.line1).toEqual("This is line one");
+      expect(haikuCheck.line2).toEqual("This is line two");
+      expect(haikuCheck.line3).toEqual("This is line three");
+    });
+  });
+
+  describe("checkLines", () => {
+    test("should correctly check the number of line breaks", function () {});
     let haikuCheck = new HaikuChecker(
-      "This is line one",
-      "This is line two",
-      "This is line three"
-    );
-    expect(haikuCheck.line1).toEqual("This is line one");
-    expect(haikuCheck.line2).toEqual("This is line two");
-    expect(haikuCheck.line3).toEqual("This is line three");
+        "This is line one",
+        "This is line two",
+        "This is line three"
+      );
+      expect(haikuCheck.checkLines()).toEqual(true);
   });
 });
--- src/js/haikuCheck.js
+++ src/js/haikuCheck.js
@@ -1,8 +1,10 @@
-export default class HaikuChecker{
-    constructor(line1, line2, line3) {
-        this.line1 = line1;
-        this.line2 = line2;
-        this.line3 = line3;
-    }
-}
-
+export default class HaikuChecker {
+  constructor(line1, line2, line3) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.line3 = line3;
+  }
+//   checkLines(enteredText) {
+//     // let numberOfLineBreaks = (enteredText.match(/\n/g) || []).length;
+//   }
+// }
