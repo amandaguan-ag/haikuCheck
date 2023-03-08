8beb45a8c0de1751b478e88c196314b0538edd06
amandaguan-ag <amandaguan1314@gmail.com>
2023-03-07 20:59:17 -0800

    upload WIP for Tuesday

--- src/js/haikuCheck.js
+++ src/js/haikuCheck.js
@@ -8,29 +8,34 @@ export default class HaikuChecker {
     const numLines = lineBreaks.length + 1;
     return numLines;
   }
-//   var count = function(word) 
-// {
-//     word = word.toLowerCase();                                     
-//     word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '');   
-//     word = word.replace(/^y/, '');                                 
-//     //return word.match(/[aeiouy]{1,2}/g).length;   
-//     var syl = word.match(/[aeiouy]{1,2}/g);
-//     console.log(syl);
-//     if(syl)
-//     {
-//         //console.log(syl);
-//         return syl.length;
-//     }
+  //   var count = function(word)
+  // {
+  //     word = word.toLowerCase();
+  //     word = word.replace(/(?:[^laeiouy]|ed|[^laeiouy]e)$/, '');
+  //     word = word.replace(/^y/, '');
+  //     //return word.match(/[aeiouy]{1,2}/g).length;
+  //     var syl = word.match(/[aeiouy]{1,2}/g);
+  //     console.log(syl);
+  //     if(syl)
+  //     {
+  //         //console.log(syl);
+  //         return syl.length;
+  //     }
 
-//     // checkSyllables() {
-//     //     const lines = this.input.split('\n');
-//     //     const syllablesPerLine = [];
-//     //     const syllableRegex = /[aeiouy]+/gi;
-//     //     for (let line of lines) {
-//     //       const syllables = line.match(syllableRegex);
-//     //       const syllableCount = syllables ? syllables.length : 0;
-//     //       syllablesPerLine.push(syllableCount);
-//     //     }
-//     //     return syllablesPerLine;
-//     //   }
+  checkSyllables() {
+    const lines = this.input.split("\n");
+    const syllableCounts = lines.map((line) => countSyllables(line));
+    return syllableCounts[0] === 5;
+  }
+}
+function countSyllables(word) {
+  // A very simple syllable counter that just counts the number of vowels in a word.
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
 }
